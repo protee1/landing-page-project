65ee1205551ad05d851015a92d555525332947db
protee <inezaprotogene@gmail.com>
2021-06-25 08:28:39 +0200

    last modification

--- main.js
+++ main.js
@@ -1,13 +1,46 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const navLink = document.querySelector(".nav-link");
-hamburger.addEventListener("click", mobileMenu);
-navLink.forEach((n) => n.addEventListener("click", closeMenu));
-function mobileMenu() {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-}
-function closeMenu() {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}
+const nav_list = document.querySelectorAll(".list");
+const sub_list = document.querySelectorAll(".sub_list");
+const openBtn = document.querySelector(".humberger");
+const navigationBox = document.querySelector(".nav_one");
+
+nav_list.forEach((list) => {
+  list.addEventListener("mouseenter", (e) => {
+    const listIcon = list.querySelector(".fas");
+    listIcon.classList.remove("fa-chevron-down");
+    listIcon.classList.add("fa-chevron-up");
+    const requiredList = list.lastElementChild;
+    if (requiredList.classList.contains("sub_list")) {
+      requiredList.style.display = "flex";
+    }
+  });
+});
+nav_list.forEach((list) => {
+  list.addEventListener("mouseleave", () => {
+    const listIcon = list.querySelector(".fas");
+    listIcon.classList.remove("fa-chevron-up");
+    listIcon.classList.add("fa-chevron-down");
+    const requiredList = list.lastElementChild;
+    if (requiredList.classList.contains("sub_list")) {
+      requiredList.style.display = "none";
+    }
+  });
+});
+
+openBtn.addEventListener("click", (e) => {
+  if (e.target.classList.contains("humberger")) {
+    navigationBox.style.display = "grid";
+    openBtn.src = "./utils/images/icon-close.svg";
+    openBtn.classList.add("opened");
+    e.target.addEventListener("click", (e) => {
+      if (e.target.classList.contains("opened")) {
+        navigationBox.style.display = "none";
+        openBtn.src = "./utils/images/icon-hamburger.svg";
+        openBtn.classList.remove("opened");
+      } else {
+        navigationBox.style.display = "grid";
+        openBtn.src = "./utils/images/icon-close.svg";
+        openBtn.classList.add("opened");
+      }
+    });
+  }
+});
